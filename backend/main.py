3b48e966d4cc3b7b28f8bd10fddefd5a1c896c85
PARTH PATIL <parthpatil5012@gmail.com>
2025-05-14 18:29:40 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -23,6 +23,7 @@ initialize_db()
 UPLOAD_DIR = "tmp/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
+#upload api to upload the file in the above folder and store the value in the database with the databeing extracted tooo
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
     file_path = os.path.join(UPLOAD_DIR, file.filename)
@@ -35,10 +36,12 @@ async def upload_file(file: UploadFile = File(...)):
         "filename": file.filename
     }
 
+#gets the pdf info
 @app.get("/pdfs")
 def list_pdfs():
     return get_pdf()
 
+#the psot method of the question asked by the user to ai for the given pdf name
 @app.post("/ask")
 async def ask_question(data: dict):
     filename = data.get("filename")
